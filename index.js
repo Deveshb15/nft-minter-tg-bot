36bd0c6b7c1bb6fe0e03f398279320819af17b25
deveshb-am <devesh.b@asset.money>
2023-06-18 19:54:13 +0530

    fix: minting exp

--- index.js
+++ index.js
@@ -12,55 +12,107 @@ const bot = new TelegramBot(token, { polling: true });
 
 console.log("Bot has started");
 
-bot.onText(/\/gt (.+)/, async (msg, match) => {
+const sleep = (ms) => {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
+
+bot.on("message", (msg) => {
   const chatId = msg.chat.id;
-  const resp = match[1];
+  const text = msg.text.toLowerCase();
 
-  console.log("Input ", resp);
-  const output = await replicate.run(
-    "stability-ai/stable-diffusion:db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf",
-    {
-      input: {
-        prompt: resp,
-      },
-    }
-  );
-  console.log("Output ", output);
+  if (text === "/start" || text === "/hello") {
+    // Greeting message
+    const welcomeMessage =
+      "Welcome to the Minter bot!\n\n" +
+      "You can mint an AI generated image as an NFT\n\n" +
+      "Just add your wallet address and a prompt to generate image" +
+      "Please enter your message or command.\n\n" +
+      "/gt <wallet address> <prompt>";
+    bot.sendMessage(chatId, welcomeMessage);
+  } else if (text === "/help") {
+    // Help message
+    const helpMessage =
+      "You can use the command below\n\n" + "/gt <wallet address> <prompt>";
+    bot.sendMessage(chatId, helpMessage);
+  }
+});
 
-    const mint_res = await axios.post('https://api.nftport.xyz/v0/mints/easy/urls', {
-        chain: 'polygon',
+bot.onText(/\/gt (.+)/, async (msg, match) => {
+  try {
+    const chatId = msg.chat.id;
+    const message_string = match[1];
+    const message_array = message_string.split(" ");
+    const wallet = message_array[0];
+    const resp = message_array.slice(1).join(" ");
+
+    console.log("Wallet address ", wallet);
+    console.log("Prompt ", resp);
+    // add a loading message
+    const loading_message = await bot.sendMessage(chatId, "Generating NFT...");
+
+    console.log("Input ", resp);
+    const output = await replicate.run(
+      "stability-ai/stable-diffusion:db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf",
+      {
+        input: {
+          prompt: resp,
+        },
+      }
+    );
+    console.log("Output ", output);
+
+    const mint_res = await axios.post(
+      "https://api.nftport.xyz/v0/mints/easy/urls",
+      {
+        chain: "polygon",
         name: resp,
-        description: 'built with love by devesh',
+        description: "built with love by devesh",
         file_url: output[0],
-        mint_to_address: '0xE964d1ad040334E863a1c5D03594Aa59F1Abea2b'
-      }, {
+        mint_to_address: wallet,
+      },
+      {
         headers: {
           Authorization: process.env.NFTPORT_API_TOKEN,
-          Accept: 'application/json',
-          'Content-Type': 'application/json'
-        }
-      })
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+      }
+    );
 
-      const mint_data = await mint_res.data
-      console.log("Mint data ", mint_data)
+    const mint_data = await mint_res.data;
+    console.log("Mint data ", mint_data);
 
-      const txn_res = await axios.get(`https://api.nftport.xyz/v0/mints/${mint_data.transaction_hash}?chain=polygon`, {
+    // await sleep(5000);
+    let txn_res;
+    let txn_data;
+    let counter = 0;
+    await sleep(5000);
+    txn_res = await axios.get(
+      `https://api.nftport.xyz/v0/mints/${mint_data.transaction_hash}?chain=polygon`,
+      {
         headers: {
           Authorization: process.env.NFTPORT_API_TOKEN,
-          Accept: 'application/json'
-        }
-      })
-      const txn_data = await txn_res.data
-      console.log("Txn data ", txn_data)
-      const minted_url = `https://opensea.io/assets/matic/${txn_data.contract_address}/${txn_data.token_id}`
+          Accept: "application/json",
+        },
+      }
+    );
 
-      const caption = `
-      Prompt: ${resp}
-      Minted NFT: ${minted_url}
-      `
+    txn_data = await txn_res.data;
 
-  bot.sendPhoto(chatId, output[0], {caption: caption});
+    console.log("Txn data ", txn_data);
+    const minted_url = `https://opensea.io/assets/matic/${txn_data.contract_address}/${txn_data.token_id}`;
 
+    const caption = `
+Prompt: ${resp}
+Minted NFT: ${minted_url}
+`;
+
+    // delete the loading message
+    bot.deleteMessage(chatId, loading_message.message_id);
+    bot.sendPhoto(chatId, output[0], { caption: caption });
+  } catch (err) {
+    console.log(err.message);
+  }
 });
 
 // 0xE964d1ad040334E863a1c5D03594Aa59F1Abea2b
